29ad4e87aee6893178e4d92cd66bb71e58b7f42d
mr-delorenzi <none@none>
2017-09-24 12:42:31 +0100

    fix bug

--- couch/design/views/webmarks-by-date/map.js
+++ couch/design/views/webmarks-by-date/map.js
@@ -2,7 +2,7 @@ function(doc) {
     // !code  lib/fragments/isDesignDoc.js
     
     if (!is_Design() && doc.type == "webmark"){
-	emitLinks( [doc.unixdate] ,doc);
+	emit( [doc.unix_date] ,doc);
     }
 }
 
