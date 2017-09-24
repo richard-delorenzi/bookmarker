6e446ff41618a980b14fc2db811534a2b7da1b3a
mr-delorenzi <none@none>
2017-09-24 13:22:30 +0100

    add by-tag view

--- couch/design/views/webmarks-by-date/map.js
+++ couch/design/views/webmarks-by-date/map.js
@@ -2,7 +2,7 @@ function(doc) {
     // !code  lib/fragments/isDesignDoc.js
     
     if (!is_Design() && doc.type == "webmark"){
-	emit( [doc.unix_date] ,doc);
+	emit( [doc.unix_date] ,null);
     }
 }
 
