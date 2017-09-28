350eb626cb8186cf19efe128604d102cd6ff8efd
mr-delorenzi <none@none>
2017-09-28 11:29:04 +0100

    adding add page

---
+++ couch/design/lists/add-bookmark.js
@@ -0,0 +1,32 @@
+"use strict";
+function (head, req) {
+    const ddoc = this;
+    const Mustache = require("lib/mustache");
+    const List = require("vendor/couchapp/lib/list");
+    const path = require("vendor/couchapp/lib/path").init(req);
+    const myLib = require("lib/myLib");
+
+//-----------------------------------------------------------------------------------------
+
+    const template = req.query["template"];
+    //const key = req.query["key"];
+    const title = req.query["title_part1"]+ " " +req.query["title_part2"] ;
+
+    function stash(){
+        return {title:title};
+    }
+
+
+
+    //-- The provides function serves the format the client requests.
+    //-- The first matching format is sent, so reordering functions changes 
+    //-- thier priority. In this case HTML is the preferred format, so it comes first.
+
+    provides("html", function() {
+        return Mustache.to_html(ddoc.templates[template],  myLib.addIfdef(stash()), ddoc.templates.partials);
+    });
+
+    provides("json", function() {
+        return JSON.stringify(stash());
+    });   
+};
