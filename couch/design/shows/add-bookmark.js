8c2f37ee94780e760922f2caaf25aeeee5b89d1a
mr-delorenzi <none@none>
2017-10-11 11:26:39 +0100

    adding edit prefill
    --HG--
    branch : feature. edit bookmark

--- couch/design/shows/add-bookmark.js
+++ couch/design/shows/add-bookmark.js
@@ -25,13 +25,17 @@ function (doc, req) {
 	};
 
 	if (docId == null){
-	    stash.bm_title= req.query["title"];
+	    stash.bm_name= req.query["title"];
 	    stash.bm_url=req.query["url"];
 	    stash.bm_description=req.query["description"];
 	}else{
-	    stash.bm_title= req.query["title"];
-	    stash.bm_url=req.query["url"];
-	    stash.bm_description=req.query["description"];
+	    stash.bm_author=doc["author"];
+	    stash.bm_created_at=doc["created_at"];
+	    stash.bm_is_private=doc["is_private"];
+	    stash.bm_name= doc["name"];
+	    stash.bm_tags=doc["tags"];
+	    stash.bm_url=doc["url"];
+	    stash.bm_description=doc["description"];    
 	}
 	return stash;
     }
