a99b5c2503b34534005dcb49ed4d442128573ec4
mr-delorenzi <none@none>
2018-02-15 16:53:38 +0000

    fix to tag showing page, to group by date

--- couch/design/views/webmarks-by-tag/map.js
+++ couch/design/views/webmarks-by-tag/map.js
@@ -4,8 +4,10 @@ function(doc) {
     
     if (!is_Design() && doc.type == "webmark"){
 	const tags = doc.tags;
+	const date_time = doc.created_at;
+	const date=date_time.split("T")[0];
 	tags.forEach ( function (tag) {
-	    emit([tag.toLowerCase()],{count:1});
+	    emit([tag.toLowerCase(),date],{count:1});
 	});
     }
 }
